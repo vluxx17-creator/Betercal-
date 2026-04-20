32061a6836173f7a21dae63bbe9a2c16ca217038
vluxx17-creator <vluxx17@gmail.com>
2026-04-20 22:16:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -121,3 +121,6 @@ def handle_search():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+    @app.route('/')
+def home():
+    return "СЕРВЕР РАБОТАЕТ! ЖДУ ЗАПРОСОВ."
